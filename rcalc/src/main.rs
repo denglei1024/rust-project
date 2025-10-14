c947809fa0577573fe0727581ed0e6af2f719a0c
denglei1024 <denglei1024@hotmail.com>
2025-10-14 21:57:40 +0800

    feat: create calc tool

--- rcalc/src/main.rs
+++ rcalc/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let expression = args.expression;
+    match meval::eval_str(expression) {
+        Ok(result) => println!("Result: {}", result),
+        Err(e) => eprintln!("Error evaluating expression: {}", e),
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    expression: String
 }
